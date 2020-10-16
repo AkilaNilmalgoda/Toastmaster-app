ea5f3e5c384d774b46ce7cbf2b399adafd6c12da
AkilaNilmalgoda <akilanilmalgoda2@hmail.com>
2020-10-17 01:09:46 +0300

    Responsiveness improved

--- src/components/QNDunit.js
+++ src/components/QNDunit.js
@@ -17,12 +17,13 @@ export default class QNDunit extends Component {
         <div className="row col-12 QNdisplaytitle  d-flex flex-wrap">
           <h1>{title}</h1>
         </div>
-        <div className="QNdisplayinput col-6">
+        <div className="QNdisplayinput col-9">
           <form onSubmit={handleSpeakerSubmit}>
-            <div className="input-group m-auto">
+            <div className="input-group m-auto ">
+
               <input
                 type="text"
-                className="form-control  "
+                className="form-control col-md-9 col-sm-12 mt-2 "
                 placeholder="Speaker's name"
                 aria-label="Recipient's username"
                 aria-describedby="button-addon2"
@@ -30,8 +31,8 @@ export default class QNDunit extends Component {
                 value={speaker}
                 onChange={handleSpeakerChange}
               ></input>
-              <div className="input-group-append ">
-                <button type="submit" className="btn btn-outline-secondary ">
+              <div className="input-group-append col-md-3 col-sm-12 mt-2 ">
+                <button type="submit" className="btn btn-outline-secondary mx-auto  ">
                   Add name
                 </button>
               </div>
