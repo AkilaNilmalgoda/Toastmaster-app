1dca4d392f0b09300f2b29301cbd485c1999181f
AkilaNilmalgoda <akilanilmalgoda2@hmail.com>
2020-10-16 10:01:52 +0300

    mq change

--- src/components/AddQ.js
+++ src/components/AddQ.js
@@ -37,10 +37,10 @@ this.setState({
         const {handleChange, handleSubmit, } = this.context;
         
         return (
-          <div className="container">
+          <div className="container-fluid">
             <form className="mt-2" onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="input col-9 sm-col-12 input-group mb-3 mt-2">
+              <div className="row ">
+                <div className="input col-md-9 col-sm-12 input-group mb-3 mt-2">
                   <div className="col-12">
                     <input
                       className="col-12 form-control"
@@ -54,10 +54,10 @@ this.setState({
                     ></input>
                   </div>
                 </div>
-                <div className="submit col-3 sm-col-12 mt-1 ">
+                <div className="submit col-md-3 col-sm-12 mt-1 ">
                   <button
                     type="submit"
-                    className="btn btn-lg btn-block addqbutton questionsubmit font-weight-bold"
+                    className="btn btn-lg btn-block addqbutton questionsubmit font-weight-bold mx-auto"
                   >
                     Submit
                   </button>
