38f6839faef0f42f16998df0583e12026e6306af
AkilaNilmalgoda <akilanilmalgoda2@hmail.com>
2020-10-18 13:23:51 +0300

    ad-editing 2

--- src/components/Advertising.js
+++ src/components/Advertising.js
@@ -6,15 +6,15 @@ export default class Advertising extends Component {
         return (
             <section>
 
-                <div className="row mx-auto d-flex justify-content-around advert-background" >
+                <div className="row mx-auto d-flex justify-content-around fixed-bottom advert-background" >
                     
                     
-                    <img className="ad-logo" src={MFSCLogo} alt="MFSCLogo"/>
+                    <img className="ad-logo mx-auto" src={MFSCLogo} alt="MFSCLogo"/>
                        
-                    <h4 className="col-md-5 col-sm-12 mx-auto text-wrap" >Join Moscow Free Speakers Club</h4>
+                    <h4 className="col-md-5 col-sm-12 mx-auto text-center text-wrap" >Join Moscow Free Speakers Club</h4>
                     
                     <h4 className="col-md-3 col-sm-12 mx-auto text-center  text-wrap"> Every other Monday @ 19.00</h4>
-                    <h4 className="col-md-3 col-sm-12 mx-auto text-wrap" >moscowfreespeakers.ru</h4>
+                    <h4 className="col-md-3 col-sm-12 mx-auto text-center text-wrap" >moscowfreespeakers.ru</h4>
                     
                 </div>              
             
